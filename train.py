94eefffd54fc9942b750dd43b8338ec319b47fb3
David Kirwan <david@davk.me>
2023-12-12 12:04:10 +0000

    Updating project

--- train.py
+++ train.py
@@ -20,8 +20,14 @@ del df['album_name']
 del df['track_name']
 del df['popularity']
 del df['explicit']
+del df['key']
+del df['mode']
+del df['time_signature']
 
 df.rename(columns = {'track_genre':'genre'}, inplace = True)
+
+drop_genres = ['acoustic','alternative','afrobeat','alt-rock','ambient','anime','black-metal','bluegrass','blues','brazil','breakbeat','british','cantopop','chicago-house','children','chill','club','comedy','dancehall','death-metal','deep-house','detroit-techno','disney','drum-and-bass','dub','dubstep','edm','electro','emo','folk','forro','french','funk','garage','german','gospel','goth','grindcore','groove','grunge','guitar','happy','hard-rock','hardcore','hardstyle','heavy-metal','honky-tonk','idm','indian','indie-pop','indie','industrial','iranian','j-dance','j-idol','j-pop','j-rock','k-pop','kids','latin','latino','malay','mandopop','metalcore','minimal-techno','mpb','new-age','opera','pagode','party','piano','pop-film','power-pop','progressive-house','psych-rock','punk-rock','punk','reggae','reggaeton','rock-n-roll','rockabilly','romance','sad','salsa','samba','sertanejo','show-tunes','singer-songwriter','ska','sleep','songwriter','soul','spanish','study','swedish','synth-pop','tango','techno','trance','trip-hop','turkish','world-music']
+df.drop(df[df['genre'].isin(drop_genres)].index, inplace = True)
 
 # Training the model on the full dataset
 df_train_full, df_test = train_test_split(df, test_size=0.2, random_state=1)
@@ -37,9 +43,9 @@ X_train_full = dv.fit_transform(dicts_train_full)
 le = LabelEncoder()
 y_train_full = le.fit_transform(y_train_full)
 
-rf = RandomForestClassifier(n_estimators=175,
+rf = RandomForestClassifier(n_estimators=125,
                             max_depth=15,
-                            min_samples_leaf=5,
+                            min_samples_leaf=3,
                             random_state=1)
 
 rf.fit(X_train_full, y_train_full)
